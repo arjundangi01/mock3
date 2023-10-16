ab90840e6aa6e80f4ed2ccd256c748c202226bb9
arjundangi01 <arjundangi907473@gmail.com>
2023-10-16 23:55:18 +0530

    url updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT
 const app = express();
 
 app.use(cors({
-    origin:['http://localhost:3000']
+    origin:['http://localhost:3000','https://graceful-kheer-2829d8.netlify.app']
 }))
 app.use(express.json())
 
